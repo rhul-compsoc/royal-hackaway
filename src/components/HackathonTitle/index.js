12d89b6367daec3f2cf3c9678db1c854fe495b0d
Leondro Lio <github@leondrolio.com>
2020-10-01 14:09:30 +0100

    Run formatter

--- src/components/HackathonTitle/index.js
+++ src/components/HackathonTitle/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react"
 import styles from "./index.module.scss"
 import { CombineStyles } from "../../helpers/CombineStyles"
-import image from './image.svg';
-
+import image from "./image.svg"
 
 class HackathonTitle extends Component {
   render() {
@@ -12,9 +11,18 @@ class HackathonTitle extends Component {
           <div className="col-3 text-left py-5">
             <div>
               <h1 className="py-2">Create something awesome</h1>
-              <p className="py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et auctor neque, id ornare nibh. Aliquam id velit aliquet, tristique ligula sit amet, dictum mauris. </p>
-              <p>Puis condimentum tristique libero, ac imperdiet mauris vulputate ut. Pellentesque tellus sem, hendrerit vel orci vitae, euismod lobortis lectus. Quisque odio mi, faucibus at neque blandit, aliquam tincidunt sapien.</p>
-              </div>
+              <p className="py-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse et auctor neque, id ornare nibh. Aliquam id velit
+                aliquet, tristique ligula sit amet, dictum mauris.{" "}
+              </p>
+              <p>
+                Puis condimentum tristique libero, ac imperdiet mauris vulputate
+                ut. Pellentesque tellus sem, hendrerit vel orci vitae, euismod
+                lobortis lectus. Quisque odio mi, faucibus at neque blandit,
+                aliquam tincidunt sapien.
+              </p>
+            </div>
           </div>
           <div className="col-3">
             <div className="mx-auto">
