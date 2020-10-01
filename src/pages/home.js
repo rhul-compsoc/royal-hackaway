f0d0564339cbab4afbd98e8a5c8b4d625fd34469
aejb <ash@aejb.host>
2020-10-01 12:29:31 +0100

    removes hackawayv4 template

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { ComingSoon } from "../components/ComingSoon"
 import { Layout } from "../components/Layout"
 import { SEO } from "../components/SEO"
 import { HackathonDefinition } from "../components/HackathonDefinition"
